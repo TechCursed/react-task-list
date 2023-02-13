5a62fea39e98e97e2f69d469c5cb25eb4b6ec742
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-02-13 18:38:17 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState} from 'react';
+
+const App = () => {
+  
+  //for storing the current form item
+  const [item, setItem] = useState('');
+
+  //for storing the current submitted form item in the main array
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+
+    console.log('handleSubmit ran');
+    
+    // prevent page refresh
+    e.preventDefault(); 
+     
+    if(item!=='')
+    {
+      setList([{id: `${item}-${Date.now()}`, item}, ...list])
+    }
+
+    // "${item}-${Date.now()" gives unique ID 
+    // "..." is the spread operator
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/* form has the method onsubmit for post submission functionalities  */}
+      
+      <form onSubmit={handleSubmit}>
+
+        <input type="text" 
+        placeholder="Enter Items" 
+
+        // onchange is required to eep track of entered values in the form
+
+        onChange={(e) => setItem(e.target.value)}
+        />
+
+        <button type="submit">Add Items</button>
+      </form>
+
+      {
+        list.map((li) => {
+          return <h3>{li.item}</h3>
+        })
+      }
+
     </div>
   );
-}
+};
 
 export default App;
+
