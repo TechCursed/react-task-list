d4011bfb259b3a1b264b76a7da7596c1b3b805e2
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-02-14 17:21:58 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import {useState} from 'react';
+import uuid from 'react-uuid';
 import './App.css'
 
 const App = () => {
@@ -8,18 +9,24 @@ const App = () => {
   const [item, setItem] = useState('');
 
   //for storing the current submitted form item in the main array
-  const [list, setList] = useState([]);
+const [list, setList] = useState([
+  {id: uuid(), item: "Go out for a walk"},
+  {id: uuid(), item: "Read React Fundamentals"},
+  {id: uuid(), item: "Practice Guitar"},
+  {id: uuid(), item: "Buy Groceries for the Dinner"},
+  {id: uuid(), item: "Study Computer Fundamentals"},
+]);
 
   const handleSubmit = (e) => {
-
-    console.log('handleSubmit ran');
     
     // prevent page refresh
     e.preventDefault(); 
      
     if(item!=='')
     {
-      setList([{id: `${item}-${Date.now()}`, item}, ...list])
+      setList([{id: uuid(), item}, ...list]);
+
+      setItem('');
     }
 
     // "${item}-${Date.now()" gives unique ID 
@@ -27,39 +34,45 @@ const App = () => {
 
   };
 
+  const handleDelete = (id) =>{
+    //filters the list based on id which is to be deleted
+    const deleteList = list.filter((to) => to.id !== id);
+
+    // set the current state to display deleted item by passing the filtered array to setList
+    setList([...deleteList])
+  }
+
   return (
 
     <div className="App">
       <div className="container">
 
-       <h1> React Tasks App </h1>
-        <form  onSubmit={handleSubmit}>
+       <h2> React Tasks App </h2>
+        <form  onSubmit={handleSubmit} className="form-div">
 
     <input 
     className="text-inp"
     type="text" 
     placeholder="Enter Items" 
-
+    value={item}
     // onchange is required to eep track of entered values in the form
 
     onChange={(e) => setItem(e.target.value)}
     />
 
-  <button className="btn-submit" type="submit">Add Items</button>
+  <button className="btn-submit" type="submit" > ADD ITEMS </button>
   </form>
 
     {
       list.map((li) => {
       return ( 
         <ul className="item-list">
-          <li className='single'>
+          <li className='single-li'>
           <span className='list-text'>{li.item}</span>
-          <button className="btn-edit" type="submit">Edit</button>
-          <button className="btn-delete" type="submit">Delete</button>
+          <button className="btn-delete" type="submit" onClick={()=>handleDelete(li.id)}> DELETE </button>
           </li>
 
         </ul>
-    
   )
 })
 } 
